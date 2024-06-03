fdf909fd614f093da0bf2411b148a11a9b27d644
Manoj Hansda <manojhansda@mplgaming.com>
2024-06-03 21:10:13 +0530

    Add Banner Images

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,54 @@
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const BannerImages = [
+  '/images/banners/banner-image-1.jpeg',
+  '/images/banners/banner-image-2.jpeg',
+  '/images/banners/banner-image-3.jpeg',
+  '/images/banners/banner-image-4.jpeg',
+  '/images/banners/banner-image-5.jpeg',
+  '/images/banners/banner-image-6.jpeg',
+  '/images/banners/banner-image-7.jpeg',
+  '/images/banners/banner-image-8.jpeg',
+  '/images/banners/banner-image-9.jpeg',
+  '/images/banners/banner-image-10.jpeg',
+  '/images/banners/banner-image-11.jpeg',
+  '/images/banners/banner-image-12.jpeg',
+  '/images/banners/banner-image-13.jpeg',
+];
+
+const Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+};
+
 const Banner = () => {
   return (
     <>
-      <div style={{ height: 500, overflow: 'hidden' }}>
-        <img src="https://picsum.photos/1000" alt="" width="100%" height="300px" />
-      </div>  
+      <Slider {...Settings}>
+        {BannerImages.map((img) => (
+          <div key={img}>
+            <div
+              style={{
+                backgroundImage: `url(${img})`,
+                height: '500px',
+
+                width: '100%',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            />
+          </div>
+        ))}
+      </Slider>
     </>
-  )
-}
+  );
+};
 
 export default Banner;
--- src/utils/getDirImages.ts
+++ src/utils/getDirImages.ts
@@ -3,17 +3,22 @@ import fs from 'fs';
 import path from 'path';
 
 const getDirImages = (directoryPath: string) => {
-  const files = fs.readdirSync(`./public${directoryPath}`);
+  try {
+    const files = fs.readdirSync(`./public${directoryPath}`);
 
-  const imageFiles = files
-    .filter((file) =>
-      ['.jpg', '.jpeg', '.png'].includes(path.extname(file).toLowerCase())
-    )
-    .map((img) => ({
-      src: `${directoryPath}/${img}`,
-    }));
+    const imageFiles = files
+      .filter((file) =>
+        ['.jpg', '.jpeg', '.png'].includes(path.extname(file).toLowerCase())
+      )
+      .map((img) => ({
+        src: `${directoryPath}/${img}`,
+      }));
 
-  return imageFiles;
+    return imageFiles;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 };
 
 export default getDirImages;
